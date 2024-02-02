b30c11db899331281efb89ec3dc58b3b9d475c18
Shrey Singh <125403535+9shrey@users.noreply.github.com>
2024-02-02 10:44:05 +0530

    Update q2.py

--- Lab4/q2.py
+++ Lab4/q2.py
@@ -1,30 +1,43 @@
 class Graph:
     def __init__(self):
         self.graph = {}
+        self.list = []
 
     def addEdge(self, u, v):
         if u in self.graph:
             self.graph[u].append(v)
         else:
             self.graph[u] = [v]
+        if v in self.graph:
+            self.graph[v].append(u)
+        else:
+            self.graph[v] = [u]
+        if v in self.list:
+            print("Cyclic Graph")
+            exit(0)
+        self.list.append(v)
 
     def BFS(self, v):
         visited = set()
         queue = [v]
-        visited.add(v)
 
         while queue:
             vertex = queue.pop(0)
-            print(vertex, end=' ')
-
+            self.list.append(vertex)
             for n in self.graph[vertex]:
                 if n not in visited:
                     queue.append(n)
                     visited.add(n)
 
+        print("Acyclic Graph")
 
 if __name__ == "__main__":
     g = Graph()
+#     g.addEdge(3, 1)
+#     g.addEdge(1, 4)
+#     g.addEdge(4, 5)
+#     g.addEdge(5, 6)
+#     g.addEdge(2, 3)
     g.addEdge(0, 1)
     g.addEdge(0, 2)
     g.addEdge(1, 2)
@@ -32,6 +45,6 @@ if __name__ == "__main__":
     g.addEdge(2, 3)
     g.addEdge(3, 3)
 
-    print("Following is Breadth First Traversal (starting from vertex 2)")
+    print()
     g.BFS(2)
     print()
